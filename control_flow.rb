3b1ccd8e5b29168629d9e773e3d7b79a1079f93b
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-18 14:33:20 +0300

    commit tree 1bb559b9d471622b503cbb92831bacbcd05d0fe8

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,50 @@
 def admin_login(username, password)
+  if username == "ADMIN" && password == "12345"
+     return "Access granted"
+  elsif username == "admin" && password == "12345"
+      return "Access granted"
+  else 
+    return "Access denied"
+  end
   # your code here
 end
 
 def hows_the_weather(temperature)
+  if temperature<40
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return  "It's a little chilly out there!"
+  elsif temperature>85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
   # your code here
 end
 
 def fizzbuzz(num)
+  if num%3 == 0 && num%5 == 0
+    return   "FizzBuzz" 
+    
+   elsif num%5 == 0
+    return  "Buzz"
+  elsif num%3 == 0
+    return "Fizz"
+  else
+    return num
+  end
   # your code here
 end
 
 def calculator(operation, num1, num2)
+   case operation
+   when "+" then num1 + num2
+   when "-" then num1-num2
+   when "*" then num1 * num2
+   when "/" then num1/num2
+   else
+    puts "Invalid operation!"
+   end
   # your code here
 end
 
